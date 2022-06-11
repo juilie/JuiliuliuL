b7022e21f29873d7fa9efc6507c374304ede4d84
julie.wav <humphreythegray@gmail.com>
2022-06-11 16:27:36 -0400

    Password progress

--- password/index.js
+++ password/index.js
@@ -6,51 +6,63 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
 camera.position.set(0, 0, 7);
-camera.lookAt(0, 0, );
+camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'), alpha: true}); // {alpha:true} for transparency
-// renderer.setClearColorHex( 0xffffff, 1 ); // assumes black background
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+    alpha: true
+}); // {alpha:true} for transparency
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement); // This is a <canvas> element
 
 
-const geometry = new THREE.ConeGeometry( 1, 2, 4 );
+const geometry = new THREE.ConeGeometry(1, 2, 4);
 geometry.faces[4].color = 0x000000;
 geometry.faces[5].color = 0x000000;
 geometry.faces[6].color = 0x000000;
 geometry.faces[7].color = 0x000000;
 
 
-const material = new THREE.MeshBasicMaterial( {vertexColors: THREE.FaceColors} );
-const cone = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshBasicMaterial({
+    vertexColors: THREE.FaceColors
+});
+const cone = new THREE.Mesh(geometry, material);
 
-cone.rotation.x=1.57;
-cone.rotation.y=.78;
-cone.rotation.z=0;
-scene.add( cone );
+cone.rotation.x = 1.5708;
+cone.rotation.y = 0.785398;
+cone.rotation.z = 0;
+scene.add(cone);
 
-function updateFaceColor(color, face){
-    if(face < 3){
-        geometry.faces[face].color.setHex( color );
+function updateFaceColor(color, face) {
+    if (face < 3) {
+        geometry.faces[face].color.setHex(color);
         geometry.colorsNeedUpdate = true;
-    }
-    else if(face ===3){
-        geometry.faces[face].color.setHex( color );
+    } else if (face === 3) {
+        geometry.faces[face].color.setHex(color);
         geometry.colorsNeedUpdate = true;
-        rotateSpeed = .01;
-    }
-    else{
+        // cone.rotation.y += .785398;
+        setTimeout(() => {
+            rotateSpeed = .03;
+        }, 1300);
+
+    } else {
         return;
     }
-
 }
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    cone.rotation.x -= rotateSpeed;
+    if (!(cone.rotation.x <= -1.5708)) {
+        cone.rotation.x -= rotateSpeed;
+    } else if (cone.position.z <= 5) {
+        cone.position.z += .05;
+    } else {
+        scene.remove(cone);
+    }
 }
+
 function onWindowResize() {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
