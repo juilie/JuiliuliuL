843fbea70a4dad9ee6b1733f994abde91def2b36
Julia Humphrey Bodian <julie@Julias-MacBook-Pro-2.local>
2023-02-22 17:42:53 -0500

    NOC perlin exercise

---
+++ NOC/PerlinNoise/sketch.js
@@ -0,0 +1,29 @@
+var DIAMETER_SLIDER
+var MULTIPLIER_SLIDER
+
+function setup() {
+  var canvas = createCanvas(displayWidth, displayHeight/2);
+  canvas.parent(document.querySelector("#canvas-wrapper"));
+
+  DIAMETER_SLIDER = document.querySelector("#diameterSlider");
+  MULTIPLIER_SLIDER = document.querySelector("#multiplierSlider");
+}
+
+
+
+yOffset = 0;
+let diameter
+let multiplier
+
+function draw() {
+  background(50)
+  var y = map(noise(yOffset), 0, 1, 0, 400);
+  yOffset += 0.01;
+
+  diameter = DIAMETER_SLIDER.value;
+  multiplier = map(MULTIPLIER_SLIDER.value, 1, 100, 0.1, 0.5);
+  
+  for (let i = 1; i < width / diameter; i++) {
+    ellipse(i * diameter, map(noise(yOffset + i ** multiplier), 0, 1, 0, height), diameter)
+  }
+}
