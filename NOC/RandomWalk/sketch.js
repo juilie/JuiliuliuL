00a533ebeefc09e04dc8e81d922fcfb70a317e23
Julia Humphrey Bodian <julie@Julias-MacBook-Pro-2.local>
2023-02-23 20:40:15 -0500

    NOC random walker

--- NOC/RandomWalk/sketch.js
+++ NOC/RandomWalk/sketch.js
@@ -1,7 +1,39 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(innerWidth, innerHeight);
+  background(50);
+  x = innerWidth/2;
+  y = innerHeight/2;
 }
-
+let randomNumber
+let STROKE_WEIGHT = 2
+let STROKE_COLOR = [255,255,255,255]
 function draw() {
-  background(220);
+  stroke(...STROKE_COLOR);
+  strokeWeight(STROKE_WEIGHT);
+
+  randomNumber = floor(random(4));
+
+  switch (randomNumber) {
+    case 0:
+      x++;
+      STROKE_COLOR[0]+=5;
+      break;
+    case 1:
+      x--;
+      STROKE_COLOR[1]+=5;
+      break;
+    case 2:
+      y++;
+      STROKE_COLOR[0]-=5;
+
+      break;
+    case 3:
+      y--;
+      STROKE_COLOR[1]-=5;
+      break;
+    default:
+      break;
+  }
+
+  point(x,y);
 }
