41812b118fa019fc8134a2086f9ce566bcd82099
Julia Humphrey Bodian <julie@Julias-MacBook-Pro-2.local>
2023-04-27 16:45:43 -0400

    randomize friends and add music

--- ps1/transitions.js
+++ ps1/transitions.js
@@ -2,12 +2,13 @@
 
 function transition(e, page) {
     e.parentElement.parentElement.classList.add('transition-out');
-    const pages = [document.getElementById('home'), document.getElementById('contact')]
+    const pages = [document.getElementById('home'), document.getElementById('contact'), document.getElementById('friends'), document.getElementById('music')]
 
     setTimeout(() => {
         e.parentElement.parentElement.classList.remove('active')
         e.parentElement.parentElement.classList.remove('active')
         e.parentElement.parentElement.classList.remove('transition-in')
+        console.log(pages[page])
         pages[page].classList.remove('transition-out')
         pages[page].classList.add('transition-in')
         pages[page].classList.add('active')
