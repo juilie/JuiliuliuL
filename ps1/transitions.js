7bed51ff48520603377dbcaaa6ea6b3f305d98bd
Julia Humphrey Bodian <julie@Julias-MacBook-Pro-2.local>
2023-04-28 00:29:59 -0400

    added websites

--- ps1/transitions.js
+++ ps1/transitions.js
@@ -9,8 +9,8 @@ function transition(e, page) {
         pages[page].classList.remove('transition-out')
         pages[page].classList.add('transition-in')
         pages[page].classList.add('active')
-        if (page === 3) {
-            let iframes = pages[3].getElementsByTagName('iframe')
+        if (page === 3 || page === 4) {
+            let iframes = pages[page].getElementsByTagName('iframe')
             for (let i = 0; i < iframes.length; i++) {
                 iframes[i].src = iframes[i].src
             }
